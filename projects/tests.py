03f9bf3ed4c5de473e264e9c0e88011506a22af1
alnibo <alexander@bode-web.eu>
2020-04-07 18:01:01 +0000

    Created first simple name test

--- projects/tests.py
+++ projects/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Project
 
-# Create your tests here.
+class ProjectTest(TestCase):
+    """
+    Here all tests are defined that
+    are run against the Project models
+    """
+
+    def test_str(self):
+        test_name = Project(name='Test Project')
+        self.assertEqual(str(test_name), 'Test Project')
