a8ffd0b56b566f2b9fa059becbb7276bd7cada1d
alnibo <alexander@bode-web.eu>
2020-04-12 16:06:14 +0000

    Added checkout app with Stripe functionality

--- django_help/settings.py
+++ django_help/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'home',
     'projects',
     'cart',
+    'checkout',
 ]
 
 MIDDLEWARE = [
@@ -135,3 +136,6 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
+STRIPE_SECRET = os.getenv('STRIPE_SECRET')
--- django_help/urls.py
+++ django_help/urls.py
@@ -20,6 +20,7 @@ from accounts import urls as urls_accounts
 from projects import urls as urls_projects
 from cart import urls as urls_cart
 from search import urls as urls_search
+from checkout import urls as urls_checkout
 from projects.views import all_projects
 from django.views import static
 from .settings import MEDIA_ROOT
@@ -31,6 +32,7 @@ urlpatterns = [
     url(r'^projects', all_projects, name='projects'),
     url(r'^projects/', include(urls_projects)),
     url(r'^cart/', include(urls_cart)),
+    url(r'^checkout/', include(urls_checkout)),
     url(r'^search/', include(urls_search)),
     url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT}),
 ]
