d8f883cec346db35eb0b925a6a983b09892a2dc4
alnibo <alexander@bode-web.eu>
2020-04-18 21:28:24 +0000

    Changed search functionality back to filter(name__icontains=request.GET['q'])

--- search/views.py
+++ search/views.py
@@ -2,14 +2,6 @@ from django.shortcuts import render
 from projects.models import Project
 
 
-# def do_search(request):
-#     projects = Project.objects.filter(name__icontains=request.GET['q'])
-#     return render(request, "projects.html", {"projects": projects})
-
-
 def do_search(request):
-    qs = Project.objects.all()
-    text = request.GET['q']
-    for term in text.split():
-        projects = qs.filter(Project(name__icontains=term) | Project(description__icontains=term))
+    projects = Project.objects.filter(name__icontains=request.GET['q'])
     return render(request, "projects.html", {"projects": projects})
