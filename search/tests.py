94eb2a454245a1dea4a9a85e5e273bc1c0840f69
alnibo <alexander@bode-web.eu>
2020-04-27 17:17:28 +0000

    Added test file to search app

--- search/tests.py
+++ search/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .views import do_search
+from projects.models import Project
+from django.contrib import auth
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class TestSearch(TestCase):
+    """test the search function"""
+    def setUp(self):
+        self.user = User.objects.create_user(username="testuser",
+                                             password="#Test1234")
+        self.client.login(username="testuser", password="#Test1234")
+
+    def test_loading_search_page(self):
+        self.user = User.objects.get(username="testuser")
+        self.project = Project(name="test")
+        self.project.save()
+        url = self.client.get('/search/?q=test')
+        self.assertEqual(url.status_code, 200)
+        self.assertTemplateUsed(url, 'projects.html')
